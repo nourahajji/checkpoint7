318277a6ba9595105b75370eb52d487302c3d400
Your Name <you@example.com>
2022-12-07 12:29:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulaire from './compenents/Formulaire';
+import Navbarr from './compenents/Navbarr';
+function App() {
+  return (
+    <div>
+     
+<Navbarr/>
+<Formulaire/>
+
+    </div>
+  );
+}
+
+export default App;
